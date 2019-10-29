b9e6ce95e6ed73f5e159468e1c44c16f5ca828f4
Mesquita <dolglas_mesquita@hotmail.com>
2019-10-29 18:00:55 -0300

    banco de dados

--- banco-de-dados.sql
+++ banco-de-dados.sql
@@ -3,8 +3,10 @@ create table tb_status(
     status varchar(25) not null
 );
 
+
 insert into tb_status(status)values('pendente');
 insert into tb_status(status)values('realizado');
+
 
 create table tb_tarefas(
 	id int not null primary key auto_increment,
